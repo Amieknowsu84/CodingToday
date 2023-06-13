caad1dc9717b3db8e9a8df1bb25fdd933789b289
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2023-06-13 22:13:24 +0530

    Time: 54 ms (33.88%), Space: 46.5 MB (93.45%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,25 +1,20 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-        int n=grid[0].length;
-        int k=0,sum=0;
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-               sum+=solve(i,0,0,j,n,grid);
-            }
+
+        Map<String,Integer> hm=new HashMap<>();
+        for(int row[]:grid){
+            hm.merge(Arrays.toString(row),1,Integer::sum);
         }
-        return sum;
-    }
-    
-    int solve(int ci,int cj,int ri,int rj,int n,int[][] grid){
-          for(int k=0;k<n;k++){
-                if(grid[ci][cj]==grid[ri][rj]){
-                    cj++;
-                    ri++;
-                }else{
-                    return 0;
-                }
-          }
-        return 1;
+        
+        int cnt=0;
+        for(int c=0;c<grid[0].length;c++){
+            int column[]=new int[grid.length];
+            for(int r=0;r<grid.length;r++){
+                column[r]=grid[r][c];
+            }
+           cnt+=hm.getOrDefault(Arrays.toString(column),0);
+        }
+        
+        return cnt;
     }
 }
