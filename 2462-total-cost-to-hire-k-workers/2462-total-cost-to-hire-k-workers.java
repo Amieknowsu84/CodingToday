c48915c9fd12fcb36a3c9439687fbcc65e71b02e
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2023-06-26 21:52:51 +0530

    Time: 63 ms (80.80%), Space: 57.9 MB (38.03%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
@@ -1,65 +1,39 @@
 class Solution {
-// //   0  1  2  3  4  5  6 7  8  9  10 11 12   
-// [-31,-25,-72,79,-74,65  ,84,   91,-18,-59,-27,-9,81,-33,-17,-58]
-// 11
-// 2
- 
-      
-      
-// // 16 len
-// //    k= 11 10 9  8 7 6 5 4 3 2 1
-// //    cost=
-// // candidates= 2
-// //       rb= 23
-// //       lb= 21
-      // 
-      
-    
     public long totalCost(int[] costs, int k, int candidates) {
-        PriorityQueue<Integer> lpq=new PriorityQueue<>();
-        PriorityQueue<Integer> rpq=new PriorityQueue<>();
-        long cost=0;
-        
-        int rb=candidates;
-        for(int i=0;i<candidates;i++){
+        PriorityQueue<Integer> lpq = new PriorityQueue<>();
+        PriorityQueue<Integer> rpq = new PriorityQueue<>();
+        long cost = 0;
+
+        int rb = candidates;
+        for (int i = 0; i < candidates; i++) {
             lpq.add(costs[i]);
         }
-        
-        int lb=costs.length-candidates-1;
-        
-        for(int i=(rb>lb)?rb:lb+1;i<costs.length;i++){
+
+        int lb = costs.length - candidates - 1;
+
+        for (int i = (rb > lb) ? rb : lb + 1; i < costs.length; i++) {
             rpq.add(costs[i]);
         }
-        
-            for(int i=0;i<k;i++){
-                
-                // System.out.println("left pq"+lpq);
-                //System.out.println("right pq"+rpq);
-                
-               
-                int leftElem=lpq.isEmpty()?Integer.MAX_VALUE:lpq.peek();
-                int rightElem=rpq.isEmpty()?Integer.MAX_VALUE:rpq.peek();
-                // System.out.println("left"+leftElem);
-               // System.out.println("right"+rightElem);
-                
-                
-                if(leftElem<=rightElem){
-                    cost+=lpq.remove();
-                    if(rb<=lb && rb<costs.length){
-                        lpq.add(costs[rb]);
-                        rb++;
-                    }
-                }else{
-                    cost+=rpq.remove();
-                    if(rb<=lb && lb>=0){
-                        rpq.add(costs[lb]);
-                        lb--;
-                    }
+
+        for (int i = 0; i < k; i++) {
+            int leftElem = lpq.isEmpty() ? Integer.MAX_VALUE : lpq.peek();
+            int rightElem = rpq.isEmpty() ? Integer.MAX_VALUE : rpq.peek();
+
+            if (leftElem <= rightElem) {
+                cost += lpq.remove();
+                if (rb <= lb && rb < costs.length) {
+                    lpq.add(costs[rb]);
+                    rb++;
+                }
+            } else {
+                cost += rpq.remove();
+                if (rb <= lb && lb >= 0) {
+                    rpq.add(costs[lb]);
+                    lb--;
                 }
             }
-            
-        
+        }
+
         return cost;
-        
     }
 }
