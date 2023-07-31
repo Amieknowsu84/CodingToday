fa87b905a842753f40f09992fc33d7c6e983fc42
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2023-07-31 22:35:34 +0530

    Time: 24 ms (92.94%), Space: 48 MB (88.60%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,10 +1,4 @@
-class Solution {
-   //   a c e
-   // a 1 1 1
-   // b 0 0 0
-   // c 0 1 1
-   // d 0 1 1 
-   // e 0 1 2   
+class Solution {  
     public int longestCommonSubsequence(String text1, String text2) {
      
         int dp[][]=new int[text1.length()+1][text2.length()+1];
@@ -13,7 +7,7 @@ class Solution {
             for(int j=0;j<=text2.length();j++){
         
                  if(i==0||j==0){
-                    dp[i][j]=1;
+                    dp[i][j]=0;
                  }else if(text1.charAt(i-1)==text2.charAt(j-1)){
                      dp[i][j]=1+dp[i-1][j-1];
                  }else{
@@ -23,7 +17,7 @@ class Solution {
             }
         }
         
-        return dp[text1.length()][text2.length()]-1;
+        return dp[text1.length()][text2.length()];
         
         
     }
