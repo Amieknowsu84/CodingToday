bcbd166791ac1a181030f26123a31e888bd3324c
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2023-08-17 19:50:30 +0530

    Time: 577 ms (7.70%), Space: 43.6 MB (9.01%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.java
@@ -0,0 +1,55 @@
+class Solution {
+    
+//      0 1 2 3 4
+//     [5,3]
+         
+//          sum=8/4
+         
+    int cnt;
+    int side;
+    HashSet<Integer> indices;
+    
+    public boolean makesquare(int[] matchsticks) {
+        cnt=0;
+        int sum=Arrays.stream(matchsticks).sum();
+        if(sum%4!=0||matchsticks.length<4){
+            return false;
+        }
+        side=sum/4;
+        indices=new HashSet<>();
+        return solve(matchsticks,side,new LinkedList<Integer>(),0);
+        
+    }
+    
+    
+    boolean solve(int[] matchsticks,int target,LinkedList<Integer> list,int index){
+        
+        if(target==0){
+            cnt++;
+            //System.out.println(list+" "+cnt);
+            indices.addAll(list);
+            boolean result=false;
+            if(cnt==4){
+               result=true; 
+            }else{
+                result = solve(matchsticks,side,new LinkedList<Integer>(),0);
+            }
+            indices.removeAll(list);
+            cnt--;
+            return result;
+        }
+        
+        for(int i=index;i<matchsticks.length;i++){
+            if(!indices.contains(i) && target>=matchsticks[i]){
+                list.add(i);
+                if(solve(matchsticks,target-matchsticks[i],list,i+1)){
+                    return true;
+                }
+                list.removeLast();
+            }
+        }
+        
+        return false;
+        
+    }
+}
