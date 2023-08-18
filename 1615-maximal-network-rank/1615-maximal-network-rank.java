5c589fb6b762281a7067f0e3fe2b816904f1e8f5
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2023-08-19 02:16:36 +0530

    Time: 18 ms (54.08%), Space: 44.5 MB (19.63%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.java
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -1,35 +1,27 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
-      List<HashSet<Integer>> adj=new ArrayList<>();
-      for(int i=0;i<n;i++){
-          adj.add(new HashSet<Integer>());
-      }
+        List<Set<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new HashSet<Integer>());
+        }
         
-      for(int edge[]:roads){
-          adj.get(edge[0]).add(edge[1]);
-          adj.get(edge[1]).add(edge[0]);
-      }  
+        for (int[] edge : roads) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }  
       
-        int max=0;
+        int max = 0;
         
-     for(int i=0;i<n;i++){
-         for(int j=i+1;j<n;j++){
-            int rankI=adj.get(i).size();
-            int rankJ=adj.get(j).size();
-            int sum=rankI+rankJ;
-            if(adj.get(i).contains(j)){
-                 sum-=1;
-             }
-             
-             max=Math.max(max,sum);
-             
-         }
-     }   
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int sum = adj.get(i).size() + adj.get(j).size();
+                if (adj.get(i).contains(j)) {
+                    sum--;
+                }
+                max = Math.max(max, sum);
+            }
+        }   
         
-        
-       return max; 
-        
-        
-        
+        return max; 
     }
 }
