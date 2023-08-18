38111b2f104d4a7b8d3abb29fd665eac3a8b99d4
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2023-08-19 02:41:49 +0530

    Time: 3 ms (87.52%), Space: 40.2 MB (64.43%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -1,50 +1,42 @@
 class Solution {
-    int target;
-    int k;
-    
     public boolean canPartitionKSubsets(int[] nums, int k) {
-        int sum=Arrays.stream(nums).sum();
-        if(sum%k!=0){
+        int sum = Arrays.stream(nums).sum();
+        if (sum % k != 0) {
             return false;
-        }else{
-            target=sum/k;
-            this.k=k;
-            //System.out.println(target);
-            Arrays.sort(nums);
-            return findSubSetSum(nums,target,0);
         }
+        
+        int target = sum / k;
+        Arrays.sort(nums);
+        int[] subsetSums = new int[k];
+        
+        return backtrack(nums, subsetSums, nums.length - 1, target);
     }
     
-     boolean findSubSetSum(int nums[],int sum,int index){
-
-          if(sum==0){
-                  boolean result=false;
-                  k--;      
-                  if(k==0){
-                    return true;
-                  }else{
-                    result = findSubSetSum(nums,target,0);
-                  }
-                  k++;
-                  return result;
-          }     
-       
+    boolean backtrack(int[] nums, int[] subsetSums, int index, int target) {
+        if (index < 0) {
+            for (int sum : subsetSums) {
+                if (sum != target) {
+                    return false;
+                }
+            }
+            return true;
+        }
         
-    
-         if(index >= nums.length){
-           return false;    
-         }  
-         
-         for(int i=index;i<nums.length;i++){
-             if(nums[i]!=-1 && sum>=nums[i]){
-                 int curr=nums[i];
-                 nums[i]=-1;
-                 if(findSubSetSum(nums,sum-curr,i+1))
-                     return true;
-                 nums[i]=curr;
-             }
-         }
-         
+        int num = nums[index];
+        for (int i = 0; i < subsetSums.length; i++) {
+            if (subsetSums[i] + num <= target) {
+                subsetSums[i] += num;
+                if (backtrack(nums, subsetSums, index - 1, target)) {
+                    return true;
+                }
+                subsetSums[i] -= num; 
+            }
+            
+            if (subsetSums[i] == 0) {
+                break; 
+            }
+        }
+        
         return false;
-     }
+    }
 }
