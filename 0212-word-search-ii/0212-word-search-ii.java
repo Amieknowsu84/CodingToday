0d19a6fee07204a8aaf39775f7682944b9ca6b90
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2023-08-19 18:21:39 +0530

    Time: 164 ms (67.85%), Space: 43.8 MB (86.23%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -0,0 +1,80 @@
+class Solution {
+    Node parent;
+    List<String> result;
+    char[][] board;
+
+    class Node{
+       char character;
+       Node child[];
+       boolean end;
+       String word; 
+        
+       Node(char character){
+           this.character=character;
+           this.child=new Node[26];
+       } 
+    }
+    
+    public void insert(String word) {
+        Node current=parent;
+        
+        for(char character: word.toCharArray()){
+            int index = character-'a';
+            if(current.child[index] == null){
+                current.child[index] = new Node(character);
+            }
+            current=current.child[index];
+        }
+        
+        current.end=true;
+        current.word=word;
+    }
+    
+    public List<String> findWords(char[][] board, String[] words) {
+        parent=new Node('.');
+        for(String word:words){
+            insert(word);
+        }
+        result=new ArrayList<>();
+        this.board=board;
+        
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                int index=board[i][j]-'a';
+                if(parent.child[index] != null){
+                    solve(i,j,parent);
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+
+    void solve(int i,int j,Node current){
+        
+        if(current!=null && current.end){
+            result.add(current.word);
+            current.end=false;
+        }
+        
+        if(i<0||j<0||j>=board[0].length||i>=board.length||board[i][j]=='$'){
+            return;
+        }
+        
+        int index=board[i][j]-'a';
+        if(current.child[index] == null){
+            return;
+        }
+        
+        char character=board[i][j];
+        board[i][j]='$';
+        solve(i+1,j,current.child[index]);
+        solve(i-1,j,current.child[index]);
+        solve(i,j+1,current.child[index]);
+        solve(i,j-1,current.child[index]);
+        board[i][j]=character;
+            
+    }
+    
+}
