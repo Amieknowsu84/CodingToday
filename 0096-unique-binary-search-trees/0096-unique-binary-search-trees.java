6bdf131ddf24193f767e7557327cfd0cdcecaa3d
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2023-08-22 19:24:15 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (82.94%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,21 +1,19 @@
 class Solution {
-    Integer dp[]=new Integer[20];
+    
     
     public int numTrees(int n) {
-        if(n <= 1){
-            return 1;
+        Integer dp[]=new Integer[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            int sum=0;
+            for(int j=1;j<=i;j++){
+                sum+=dp[j-1]*dp[i-j];
+            }
+            dp[i]=sum;
         }
         
-        if(dp[n]!=null){
-            return dp[n];
-        }
-        
-        int sum=0;
-        
-        for(int i=1;i<=n;i++){
-            sum+=numTrees(i-1)*numTrees(n-i);
-        }
-        
-        return dp[n]=sum;
+        return dp[n];
     }
 }
