86ed07d3e3948c377f3fe7c7b8c6e138cfa57fd5
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2023-08-22 23:21:54 +0530

    Time: 7 ms (15.49%), Space: 40.2 MB (29.14%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -0,0 +1,9 @@
+class Solution {
+
+    public String convertToTitle(int n) {
+        
+    return n == 0 ? "" : convertToTitle(--n / 26) + (char)('A' + (n % 26));
+            
+        
+    }
+}
