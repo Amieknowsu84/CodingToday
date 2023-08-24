7c85e525d7ec42fec43307e81bae1b1f1c5cfcf8
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2023-08-24 08:10:40 +0530

    Time: 1 ms (99.75%), Space: 43.5 MB (69.28%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -1,17 +1,40 @@
 class Solution {
     public List<TreeNode> generateTrees(int n) {
-        return dfs(1,n);
+       return construct(1,n);
     }
-    List<TreeNode> dfs(int from, int to){
-        List<TreeNode> list = new ArrayList<>();
-        if(from > to){
-            list.add(null);
-        }else {
-            for(int i = from; i <= to; i++)
-                for (TreeNode l : dfs(from, i - 1))
-                    for (TreeNode r : dfs(i + 1, to))
-                        list.add(new TreeNode(i, l, r));
+    
+    //  [1,2,3]
+    // 1,0  2,3  1,1 3,3 1,2 4,3
+    // null  [2,3 3,2] 1 3 
+    
+    List<TreeNode> construct(int start,int n){
+    
+        List<TreeNode> result=new ArrayList<TreeNode>();
+        
+        if(start>n){
+            result.add(null);
+            return result;
+        }else if(start==n){
+            result.add(new TreeNode(start,null,null));
+            return result;
         }
-        return list;
+        
+
+        for(int i=start ; i<=n ; i++){
+            
+            List<TreeNode> leftSubTree = construct(start,i-1);
+            List<TreeNode> rightSubTree = construct(i+1,n);
+        
+                
+               for(TreeNode leftTree : leftSubTree){
+                  for(TreeNode rightTree : rightSubTree){
+                      TreeNode node=new TreeNode(i,leftTree,rightTree);
+                      result.add(node);
+                  }
+               }
+                
+        }
+        
+        return result;
     }
 }
