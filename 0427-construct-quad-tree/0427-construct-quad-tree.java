ac694c40cecc7c13e0938690d2048c465f3cf598
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2023-08-26 16:40:52 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (98.19%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.java
+++ 0427-construct-quad-tree/0427-construct-quad-tree.java
@@ -46,17 +46,13 @@ class Solution {
     
      public Node construct(int[][] grid,int rs,int re,int cs,int ce) {
         if(haveSameValue(grid,rs,re,cs,ce)){
-            //System.out.println(rs+" -- "+ re+ " "+cs+ " "+ce +" "+grid[rs][cs]);
-            return new Node(grid[rs][cs]==1,true);
+            return new Node(grid[rs][cs] == 1,true);
         } 
         int size = (re-rs+1)/2;
         int midr = rs+size;
         int midc = cs+size;  
-        // (rs,rs+mid-1)  (mid,re) ;
-        //int division=rs+mid-1;
-      // System.out.println(rs+" "+ (mid-1)+ " "+mid+ " "+re);
          
-        Node current=new Node(false,false);
+        Node current = new Node(false,false);
         
         current.topLeft = construct(grid,rs,midr-1,cs,midc-1);
         current.topRight = construct(grid,rs,midr-1,midc,ce);
