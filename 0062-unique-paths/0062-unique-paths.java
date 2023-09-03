88aa8058c6e32e6d8e8e601fb5b743f94502f835
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2023-09-03 23:43:52 +0530

    Time: 1 ms (8.81%), Space: 39.3 MB (44.10%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -6,14 +6,23 @@ class Solution {
      }
     
     public int uniquePaths(int m, int n) {
+       
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }else if(i==0){
+                    dp[i][j]=j>0 ? dp[i][j-1]:0;
+                }else if(j==0){
+                    dp[i][j] = i>0? dp[i-1][j]:0;
+                }else{
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
+            }
+        }
         
-        if(m<=0|n<=0)
-            return 0;
-        if(m==1||n==1)
-            return 1;
-        if(dp[m][n]!=-1)
-            return dp[m][n];
-        return dp[m][n]=uniquePaths(m-1,n)+uniquePaths(m,n-1);
+        
+        return dp[m-1][n-1];
         
     }
 }
