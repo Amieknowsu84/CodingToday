bdb8ce43c3527ae6fb8a279b9bf157992a66e2e0
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2023-09-11 20:13:31 +0530

    Time: 17 ms (55.43%), Space: 54.1 MB (28.83%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -6,6 +6,10 @@ class Solution {
         int left = 0;
         int right = people.length - 1;
         
+        // greedy pair up min weight and max weight to get it up on board
+        // ofcourse in sorted array
+        // if sum > limit right one is boarded i.e heavy one is given seperate boat
+        
         while(left <= right){
             int sum = (left == right) ? people[left] :people[left] + people[right];
             if(sum <= limit){
