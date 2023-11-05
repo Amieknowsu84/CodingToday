3ab0f70a113585a3f05c6c622c17404a98cd018b
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2023-11-05 19:28:28 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (79.58%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -3,10 +3,10 @@ class Solution {
    if(arr.length < 3){
         return  0;    
   }
-    
+            
    int diff = arr[1]-arr[0];
-   int cnt =2;
-   ArrayList<Integer> list = new ArrayList<>();     
+   int cnt =2;  
+   int result = 0;
    
   for(int i = 2; i< arr.length; i++){
       int currDiff = arr[i]-arr[i-1];
@@ -14,22 +14,18 @@ class Solution {
          cnt++; 
       }else{
          if(cnt >= 3){
-           list.add(cnt);
+           int k = cnt- 3 +1;
+           result += (k*(k+1)/2);
          }
          cnt = 2;
          diff = currDiff;
       }   
    }
    if(cnt >= 3){
-           list.add(cnt);
+      int k = cnt- 3 +1;
+      result += (k*(k+1)/2);
   }     
-        
-    int result = 0;
-    for(int i=0; i<list.size();i++){
-        int k = list.get(i)- 3 +1;
-        result += (k*(k+1)/2);
-    }
-        
+    
     return result;    
 
 }    
