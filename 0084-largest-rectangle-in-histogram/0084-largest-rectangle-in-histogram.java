b25a33abb93b28658feaadb3bb7bc5f5f1afb9a6
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2023-12-24 12:47:09 +0530

    Time: 122 ms (8.27%), Space: 57.5 MB (30.72%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,83 +1,72 @@
+import java.util.Stack;
+
 class Solution {
-
     public int largestRectangleArea(int[] heights) {
-        int[] left=nseLeft(heights);
-        int[] right=nseRight(heights);
-        int maxArea=0;
-         
-        for(int i=0;i<heights.length;i++){
-            int maxLeft=left[i];
-            int maxRight=right[i];
-            int value=(maxRight-maxLeft-1)*heights[i];
-            maxArea= Math.max(maxArea,value);
-        } 
-         return maxArea;    
-    }
-    
-    
-      
-     int[] nseLeft(int[] heights){
-        Stack<Integer> indexes=new Stack<>();
-        int[] res=new int[heights.length];
-        
-        for(int i=0;i<heights.length;i++){
-            if(indexes.isEmpty()){
-                indexes.push(i);
-                res[i]=0;
-            }else{
-                  int ind=-1;
-                while(!indexes.isEmpty()){
-                    ind=indexes.peek();
-                    if(heights[ind]>=heights[i]){
-                       indexes.pop();
-                    }else{
-                        break;
-                    }
-                }
-               
-                if(indexes.isEmpty()){
-                    res[i]=0;
-                }else{
-                    res[i]=ind+1;
-                }
-                 indexes.push(i);
-            }
-        }
-        
-        return res;       
-    }
-    
-    
-     int[] nseRight(int[] heights){
-        Stack<Integer> indexes=new Stack<>();
-        int[] res=new int[heights.length];
-        
-        for(int i=heights.length-1;i>=0;i--){
-            if(indexes.isEmpty()){
-                indexes.push(i);
-                res[i]=heights.length+1;
-            }else{
-                int ind=-1;
-                while(!indexes.isEmpty()){
-                    ind=indexes.peek();
-                    if(heights[ind]>=heights[i]){
-                       indexes.pop();
-                    }else{
-                        break;
-                    }
-                }
+        int[] left = nseL(heights);
+        int[] right = nseR(heights);
+        int maxArea = 0;
 
-                if(indexes.isEmpty()){
-                    res[i]=heights.length+1;
-                }else{
-                    res[i]=ind+1;
-                }
-                indexes.push(i);
+        for (int i = 0; i < heights.length; i++) {
+            int value = (right[i] - left[i] - 1) * heights[i];
+            maxArea = Math.max(maxArea, value);
+        }
+
+        return maxArea;
+    }
+    
+    private int[] nseL(int heights[]){
+        Stack<Integer> stack = new Stack<Integer>();
+        int[] result = new int[heights.length];
+        int index = -1, currentIndex = -1;
+        
+        for(int i=0; i<heights.length; i++){
+            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
+                stack.pop();
             }
+            result[i] = stack.isEmpty()?-1:stack.peek();
+            stack.push(i);
         }
         
-        return res;       
+        return result;
+    }
+    
+    private int[] nseR(int heights[]){
+        Stack<Integer> stack = new Stack<Integer>();
+        int[] result = new int[heights.length];
+        
+        for(int i=heights.length-1 ; i>=0; i--){
+            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
+                stack.pop();
+            }
+            result[i] = stack.isEmpty()?heights.length:stack.peek();
+            stack.push(i);
+        }
+        
+        return result;
     }
     
     
+
+    private int[] nse(int[] heights, boolean isLeft) {
+        Stack<Integer> indexes = new Stack<>();
+        int[] res = new int[heights.length];
+        int n = heights.length;
+
+
+        int start = isLeft ? 0 : n - 1;
+        int end = isLeft ? n : -1;
+        int step = isLeft ? 1 : -1;
+
+        for (int i = start; i != end; i += step) {
+            while (!indexes.isEmpty() && heights[indexes.peek()] >= heights[i]) {
+                indexes.pop();
+            }
+
+            res[i] = isLeft ? (indexes.isEmpty() ? 0 : indexes.peek() + 1) : (indexes.isEmpty() ? n : indexes.peek());
+
+            indexes.push(i);
+        }
+
+        return res;
+    }
 }
