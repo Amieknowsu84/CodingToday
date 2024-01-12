95a473f553fb68c42d30f1cab6e52bdc3edc9bb3
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-01-12 09:16:08 +0530

    Time: 0 ms (100.00%), Space: 45 MB (5.27%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -13,21 +13,27 @@
  *     }
  * }
  */
+
 class Solution {
-    private boolean result = true;
+    public boolean isBalanced(TreeNode root) {
+        return maxDepth(root) != -1;
+    }
 
-public boolean isBalanced(TreeNode root) {
-    maxDepth(root);
-    return result;
-}
+    private int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
 
-public int maxDepth(TreeNode root) {
-    if (root == null)
-        return 0;
-    int l = maxDepth(root.left);
-    int r = maxDepth(root.right);
-    if (Math.abs(l - r) > 1)
-        result = false;
-    return 1 + Math.max(l, r);
-}
+        int leftDepth = maxDepth(root.left);
+        if (leftDepth == -1)
+            return -1;
+
+        int rightDepth = maxDepth(root.right);
+        if (rightDepth == -1)
+            return -1;
+
+        if (Math.abs(leftDepth - rightDepth) > 1)
+            return -1;
+
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
 }
