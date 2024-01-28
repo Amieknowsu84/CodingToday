3ed2ec9558d7eae8cab8421505a16d89b1221588
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-01-28 14:00:31 +0530

    Time: 83 ms (26.22%), Space: 58.5 MB (75.66%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -3,8 +3,32 @@ class Solution {
     Integer dp[][][];
     
     public int maxProfit(int[] prices) {
-        dp = new Integer[prices.length][2][3];
-        return maxProfit(prices,0,true,2);
+        // dp = new Integer[prices.length][2][3];
+        // return maxProfit(prices,0,true,2);
+        
+        
+        int[][] prev = new int[2][3];
+        int[][] curr = new int[2][3];
+        prev[1][2] = prev[1][1] = Math.max(-prices[prices.length-1],0);
+        prev[0][2] = prev[0][1] = prices[prices.length-1];
+        prev[1][0] = 0;
+        prev[0][0] = 0;
+        
+        for(int i=prices.length-2;i>=0;i--){
+            for(int j=0;j<2;j++){
+                for(int k=1;k<=2;k++)
+                  if(j == 1)
+                    curr[j][k] = Math.max(-prices[i]+prev[1-j][k],prev[j][k]);
+                  else
+                    curr[j][k] = Math.max(prices[i]+prev[1-j][k-1],prev[j][k]);
+            }
+            prev = curr;
+            curr = new int[2][3];
+            
+        }
+        
+        return prev[1][2];
+        
     }
     
      int maxProfit(int[] prices,int index,boolean buyflag,int transaction){
