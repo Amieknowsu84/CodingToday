ee3966d84c0336410bfc72cb92218f183282d8e2
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-02-06 18:23:01 +0530

    Time: 7 ms (57.75%), Space: 48 MB (19.36%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,44 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-       
-        List<List<String>> result= new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
         
-       for(String str:strs){
-           boolean added = false;   
-           for(List<String> subList:result){
-               if(hasEqualCharacters(str,subList.get(0))){
-                   subList.add(str);
-                   added = true;
-                   break;
-               }
-           }
-           
-           if(!added){
-              List<String> subList = new ArrayList<>();
-              subList.add(str);
-              result.add(subList); 
-           }   
-       }
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            
+            map.get(sortedWord).add(word);
+        }
         
-       return result; 
+        return new ArrayList<>(map.values());
 
     }
-    
-    boolean hasEqualCharacters(String a,String b){
-        if(a.length() != b.length()){
-            return false;
-        }
-        
-        int map[] = new int[26];
-        int len = a.length();
-        
-        for(int i=0;i<len;i++){
-            map[a.charAt(i)-'a']++;
-            map[b.charAt(i)-'a']--;
-        }
-        
-        return Arrays.stream(map).allMatch(elem->elem==0);
-    }
-    
     
 }
