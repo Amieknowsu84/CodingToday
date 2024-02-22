6ec97d7fcf9071e440778d632599edcebaa7f8a5
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-02-22 23:24:47 +0530

    Time: 3 ms (71.04%), Space: 53.2 MB (72.22%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,25 +1,21 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
+        int[] trustCounts = new int[n + 1];
         
-        if(n==1 && trust.length == 0){
-            return 1;
+        for (int[] tr : trust) {
+            int a = tr[0];
+            int b = tr[1];
+            trustCounts[a]--; // decrease trust count for person a
+            trustCounts[b]++; // increase trust count for person b
         }
         
-        int[] count = new int[n+1];
-        
-        for(int tr[]:trust){
-            count[tr[0]] = -1;
-            if(count[tr[1]]!=-1){
-                count[tr[1]]++;
-            }
-        }
-        
-        for(int i=1;i<=n;i++){
-            if(count[i]==n-1){
+        for (int i = 1; i <= n; i++) {
+            // If a person's net trust value is n - 1, they are the judge
+            if (trustCounts[i] == n - 1) {
                 return i;
             }
         }
         
-        return -1;
+        return -1; // judge not found
     }
 }
