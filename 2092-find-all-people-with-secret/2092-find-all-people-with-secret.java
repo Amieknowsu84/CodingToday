18acd8ed9f4e3786c4016521e01502630c349dab
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-02-25 02:00:31 +0530

    Time: 1339 ms (5.00%), Space: 106.2 MB (49.00%) - LeetHub

---
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
+        Map<Integer,List<int[]>> adjacency = new HashMap<>();
+        
+        for(int[] meeting: meetings){
+            List<int[]> list = adjacency.computeIfAbsent(meeting[0],(k)->new ArrayList<>());
+            list.add(new int[]{meeting[1],meeting[2]});
+            adjacency.put(meeting[0],list);
+            list = adjacency.computeIfAbsent(meeting[1],(k)->new ArrayList<>());
+            list.add(new int[]{meeting[0],meeting[2]});
+            adjacency.put(meeting[1],list);
+        }
+        
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a,b)->{
+            return a[1]==b[1]?(a[0]-b[0]):(a[1]-b[1]);
+        });
+        int visited[] = new int[n];
+        queue.add(new int[]{0,0});
+        queue.add(new int[]{firstPerson,0});
+        
+        Set<Integer> result = new HashSet<>();
+        
+        while(!queue.isEmpty()){
+            int[] node = queue.remove();
+            visited[node[0]] = 1;
+            //System.out.println(node[0]+" "+node[1]);
+            result.add(node[0]);
+            if(adjacency.containsKey(node[0]))
+            for(int[] adjacent:adjacency.get(node[0])){
+                if(visited[adjacent[0]]!=1 && adjacent[1]>=node[1]){
+                    //System.out.println(node[0]+"-----"+adjacent[0]);
+                    queue.add(adjacent);
+                }
+            }
+        }
+        
+        return new ArrayList<>(result);
+        
+    }
+}
