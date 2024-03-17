7178dab62912f217de2283b78ddd564fadb6b046
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-03-18 00:54:29 +0530

    Time: 2 ms (44.45%), Space: 44.6 MB (74.25%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -13,73 +13,31 @@ class Solution {
 //     1 2 3 4 5 6 7 8 9       
         
     public int[][] insert(int[][] intervals, int[] newInterval) {
-          
-        
-      ArrayList<int[]> res = new ArrayList<>();
-      int current[]= new int[2];
-      int [][] result =  new int[res.size()][];
-      
-      if(intervals.length == 0){
-          result =  new int[1][];
-          result[0] = newInterval;
-          return result;
-      }
-      current[0]=intervals[0][0];
-      current[1]=intervals[0][1];
-      boolean merged = false; 
-          
-      if(newInterval[1]<current[0]){
-          res.add(newInterval);
-          merged = true;
-      }   
-      
-         
-          
-      for(int i=0; i<intervals.length;i++){
-          
-          if((newInterval[0] >= current[0] && newInterval[0] <= current[1])||
-            (current[0] >= newInterval[0] && current[0] <= newInterval[1])){
-              current[0]=Math.min(current[0],newInterval[0]);
-              current[1]=Math.max(current[1],newInterval[1]);   
-              merged =  true;
-          }
-          
-          if((intervals[i][0] >= current[0] && intervals[i][0] <= current[1])||
-             (current[0] >= intervals[i][0] && current[0] <= intervals[i][1])){
-              current[0]=Math.min(current[0],intervals[i][0]);
-              current[1]=Math.max(current[1],intervals[i][1]); 
-          }
-          else{
-              res.add(Arrays.copyOf(current,current.length));
-              if(current[1]<newInterval[0] && intervals[i][0]>newInterval[1]){
-                  res.add(newInterval);
-                  merged = true;
-              }
-              current[0]=intervals[i][0];
-              current[1]=intervals[i][1];
-          }
-      }
-      
-    if((newInterval[0] >= current[0] && newInterval[0] <= current[1])||
-            (current[0] >= newInterval[0] && current[0] <= newInterval[1])){
-              current[0]=Math.min(current[0],newInterval[0]);
-              current[1]=Math.max(current[1],newInterval[1]);
-               res.add(current);
-        merged = true;
-     }else{
-      res.add(Arrays.copyOf(current,current.length));   
-    }
-      if(!merged){
-            res.add(newInterval);
-      }    
-          
-      result =  new int[res.size()][];
-          
-      for(int i=0;i<result.length;i++){
-          result[i]=res.get(i);
-      }    
-          
-      return result;    
-          
+       	List<int[]> result = new LinkedList<>();
+	    int i = 0;
+	    // add all the intervals ending before newInterval starts
+	    while (i < intervals.length && intervals[i][1] < newInterval[0]){
+	        result.add(intervals[i]);
+	        i++;
+	    }
+	    
+	    // merge all overlapping intervals to one considering newInterval
+	    while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+	    	// we could mutate newInterval here also
+	        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+	        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+	        i++;
+	    }
+	    
+	    // add the union of intervals we got
+	    result.add(newInterval); 
+	    
+	    // add all the rest
+	    while (i < intervals.length){
+	    	result.add(intervals[i]); 
+	    	i++;
+	    }
+	    
+	    return result.toArray(new int[result.size()][]);             
     }
 }
