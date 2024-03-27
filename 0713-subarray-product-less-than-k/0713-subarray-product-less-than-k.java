f8df7ffaf56ae802527758713827348985f553d0
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-03-27 22:31:08 +0530

    Time: 5 ms (30.94%), Space: 48 MB (9.20%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -6,19 +6,27 @@ class Solution {
 //     right 3
 //     product = 50  k=100
          
-//     [2]     
+//     [2]
+    // crux of algo
+    // j-i+1 gives all the subarray ending at j
+    //[10,5,100,6] i=2 j=3  2 subarray ending at 6  [2,6] [6]
+    
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         int cnt=0;
         long product = 1;
         int left = 0;
-        for(int right=0;right<nums.length;right++){
-            product*=nums[right];
-            while(product>=k && left<=right){
-               product/=nums[left];
-               left++;
+        int right = 0;
+        
+        while(right < nums.length){
+            product *= nums[right];
+            
+            while(product >= k && left <= right){
+                product/=nums[left];
+                left++;
             }
             
-            cnt+=(right-left+1);
+            cnt+= (right-left+1);
+            right++;
         }
         
         return cnt;         
