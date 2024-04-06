7d9d133f4a45a26c8edb5909ac2636819fe37b09
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-04-06 13:19:18 +0530

    Time: 1 ms (71.26%), Space: 46.3 MB (40.64%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,36 +2,29 @@ class Solution {
     static long trapWater(int[] heights){
         int[] left = new int[heights.length];
         int[] right = new int[heights.length];
-        long area = 0;
+        int area = 0;
 
         fillLeft(left, heights);
         fillRight(right, heights);
 
-        for (int i = 1; i < heights.length - 1; i++) {
+        for (int i = 0; i < heights.length; i++) {
             int li = left[i];
             int ri = right[i];
-            if (li >= 0 && ri >=0) {
-                //System.out.println(i+" "+li+" "+ri);
-                int height = Math.min(li,ri) - heights[i];
-                int width = 1; // fix the width calculation
-                //System.out.println("width "+width);
-                //System.out.println("height "+height);
-                area += height * width>0?height*width:0;
-            }
+            int heightOfWaterIthBuilding = Math.min(li,ri) - heights[i];
+            //System.out.println(li+" "+ri+" "+heightOfWaterIthBuilding+" "+heights[i]);
+            if(heightOfWaterIthBuilding > 0)
+             area += heightOfWaterIthBuilding;
         }
         
-        return area; // add the missing return statement
+        return area; 
     } 
     
       static void fillLeft(int[] left,int heights[]){
-       // Arrays.stream(heights).forEach((h)->System.out.print(h+" ")); 
-        int result = -1;
+        int result = heights[0];
         for(int i=0;i<left.length;i++){
             left[i] = result;
             result = Math.max(result,heights[i]);
-            //System.out.print(left[i]+" ");
         }
-        //System.out.println();
     }
      
  //     0 1 2 3 4 5 6 7 8 9 10 11
@@ -39,18 +32,13 @@ class Solution {
  //   -1-1 1-1 3 4 3-1 7 8 7  10
 //     1 3 3 7 7 6 7 12 12 10 12 12
            
-
     
     static void fillRight(int[] right,int[] heights){
-        
-        int result = -1;
+        int result = heights[heights.length-1];
         for(int i=right.length-1; i>=0; i--){
            right[i] = result;  
            result = Math.max(result,heights[i]); 
         }
-       // Arrays.stream(right).forEach((h)->System.out.print(h+" ")); 
-        //System.out.println();
-        
     }
 
 
