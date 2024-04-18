53fd0983d7cfa7f602f0912a8d6043c2f6f7b9b1
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-04-19 01:05:18 +0530

    Time: 4 ms (99.57%), Space: 46.1 MB (8.66%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,41 +1,29 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
-        int si=-1,sj=-1;
-        boolean found = false;
+         int cnt=0;
+        
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j] == 1){
-                    si = i;
-                    sj = j;
-                    found = true;
-                    break;
+                if(grid[i][j]==1){
+                    
+                    if((i-1)<0 || grid[i-1][j]==0)
+                        cnt++;
+                    if((j-1)<0 || grid[i][j-1]==0)
+                        cnt++;
+                    if((i+1) >= grid.length || grid[i+1][j]==0)
+                        cnt++;
+                     if((j+1) >= grid[0].length || grid[i][j+1]==0)
+                         cnt++;
+                    
+                    
                 }
             }
-            if(found)
-                break;
+        
         }
         
-        return islandPerimeter(si,sj,grid);
+        return cnt;
     
     }
     
-    public int islandPerimeter(int i,int j,int[][] grid) {
-        
-        if(i<0 || j<0 || i >= grid.length || j >= grid[0].length){
-            return 1;
-        }
-        
-        if(grid[i][j] == -1)
-            return 0;
-        
-        if(grid[i][j] == 0)
-            return 1;
-        
-        grid[i][j] = -1;
     
-       
-         return islandPerimeter(i+1,j,grid) +  islandPerimeter(i-1,j,grid)
-            +  islandPerimeter(i,j-1,grid) +  islandPerimeter(i,j+1,grid);
-        
-    }
 }
