6a0644b4e2ff588dd5cde2d4db2b8a4b0e8aceef
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-04-19 01:00:06 +0530

    Time: 6 ms (35.73%), Space: 45.9 MB (22.25%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -23,26 +23,20 @@ class Solution {
     public int islandPerimeter(int i,int j,int[][] grid) {
         
         if(i<0 || j<0 || i >= grid.length || j >= grid[0].length){
-            return 0;
+            return 1;
         }
         
         if(grid[i][j] == -1)
             return 0;
         
         if(grid[i][j] == 0)
-            return 0;
+            return 1;
         
         grid[i][j] = -1;
-        
-        int cnt = getCnt(i,j,grid);
+    
        
-        
-         cnt += islandPerimeter(i+1,j,grid) +  islandPerimeter(i-1,j,grid)
+         return islandPerimeter(i+1,j,grid) +  islandPerimeter(i-1,j,grid)
             +  islandPerimeter(i,j-1,grid) +  islandPerimeter(i,j+1,grid);
-        
-        
-        
-        return cnt;
         
     }
     
