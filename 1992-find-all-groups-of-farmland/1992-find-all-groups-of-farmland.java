c074c3cc68a5d201b4169ed82796beeb91ee81c0
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-04-21 01:06:37 +0530

    Time: 17 ms (39.33%), Space: 68.5 MB (35.58%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -0,0 +1,64 @@
+class Solution {
+    int r1;
+    int r2;
+    int c1;
+    int c2;
+    int max;
+    int min;
+    
+    public int[][] findFarmland(int[][] land) {
+        ArrayList<int[]> list = new ArrayList<>();
+        
+        for(int i=0; i<land.length; i++){
+            for(int j=0;j<land[0].length;j++){
+                if(land[i][j] == 1){
+                    max = Integer.MIN_VALUE;
+                    min = Integer.MAX_VALUE;
+                    //System.out.println(i+" "+j+" ");
+                    dfs(i,j,land);
+                    list.add(new int[]{r1,c1,r2,c2});
+                }
+            }
+        }
+        
+       int[][] array = new int[list.size()][];
+
+      for (int i = 0; i < list.size(); i++) {
+           array[i] = list.get(i);
+       }
+        
+       return array; 
+        
+    }
+    
+    void dfs(int i,int j,int[][] land){
+        
+        if(i<0||j<0||i>=land.length||j>=land[0].length){
+            return;
+        }
+        
+        if(land[i][j]  == 0 || land[i][j] == 2){
+            return;
+        }
+        
+        int[] dx = {0,1,0,-1};
+        int[] dy = {1,0,-1,0};
+        
+        land[i][j] = 2;
+        //System.out.println(i+" "+j+" "+max+" "+min);
+        if(i+j<min){
+            r1 = i;
+            c1 = j;
+            min = i+j;
+        }
+        if(i+j>max){
+            r2 = i;
+            c2 = j;
+            max = i+j;
+        }
+        
+        for(int k=0;k<dx.length;k++){
+            dfs(i+dx[k],j+dy[k],land);
+        }
+    }
+}
