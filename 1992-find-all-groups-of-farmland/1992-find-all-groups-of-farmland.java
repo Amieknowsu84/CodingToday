c2520c774fcd0cd065b78f18a083d156c4723e2b
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-04-21 01:17:22 +0530

    Time: 12 ms (59.55%), Space: 69.3 MB (34.08%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -1,61 +1,51 @@
 class Solution {
-    int r1;
-    int r2;
-    int c1;
-    int c2;
-    int max;
-    int min;
-    int[] dx = {0,1,0,-1};
-    int[] dy = {1,0,-1,0};
-    
+    private static final int LAND = 1;
+    private static final int VISITED_LAND = 2;
+    private static final int[] DX = {0, 1, 0, -1};
+    private static final int[] DY = {1, 0, -1, 0};
+
     public int[][] findFarmland(int[][] land) {
-        ArrayList<int[]> list = new ArrayList<>();
-        
-        for(int i=0; i<land.length; i++){
-            for(int j=0;j<land[0].length;j++){
-                if(land[i][j] == 1){
-                    max = Integer.MIN_VALUE;
-                    min = Integer.MAX_VALUE;
-                    dfs(i,j,land);
-                    list.add(new int[]{r1,c1,r2,c2});
+        ArrayList<int[]> farmlandList = new ArrayList<>();
+
+        for (int i = 0; i < land.length; i++) {
+            for (int j = 0; j < land[0].length; j++) {
+                if (land[i][j] == LAND) {
+                    int[] farmland = findFarmlandBounds(i, j, land);
+                    farmlandList.add(farmland);
                 }
             }
         }
-        
-       int[][] array = new int[list.size()][];
 
-      for (int i = 0; i < list.size(); i++) {
-           array[i] = list.get(i);
-       }
-        
-       return array; 
-        
+        return farmlandList.toArray(new int[farmlandList.size()][]);
     }
-    
-    void dfs(int i,int j,int[][] land){
-        
-        if(i<0||j<0||i>=land.length||j>=land[0].length){
+
+    private int[] findFarmlandBounds(int row, int col, int[][] land) {
+        int minRow = row, minCol = col, maxRow = row, maxCol = col;
+        int[] result = {row,col,row,col};
+
+        dfs(row, col, land, result);
+
+        return result;
+    }
+
+    private void dfs(int row, int col, int[][] land,int[] result) {
+        if (row < 0 || col < 0 || row >= land.length || col >= land[0].length || land[row][col] != LAND)          {
             return;
         }
-        
-        if(land[i][j]  == 0 || land[i][j] == 2){
-            return;
+
+        land[row][col] = VISITED_LAND;
+        if (row + col < result[0] + result[1]) {
+            result[0] = row;
+            result[1] = col;
         }
-         
-        land[i][j] = 2;
-        if(i+j<min){
-            r1 = i;
-            c1 = j;
-            min = i+j;
+
+        if (row + col > result[2] + result[3]) {
+            result[2] = row;
+            result[3] = col;
         }
-        if(i+j>max){
-            r2 = i;
-            c2 = j;
-            max = i+j;
-        }
-        
-        for(int k=0;k<dx.length;k++){
-            dfs(i+dx[k],j+dy[k],land);
+
+        for (int i = 0; i < DX.length; i++) {
+            dfs(row + DX[i], col + DY[i], land, result);
         }
     }
 }
