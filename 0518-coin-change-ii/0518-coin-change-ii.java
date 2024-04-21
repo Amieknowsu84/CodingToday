ef6318e226f949779c254d7077753f2c8fd259ee
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-04-21 12:42:15 +0530

    Time: 7 ms (51.24%), Space: 47.8 MB (35.55%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -3,9 +3,34 @@ class Solution {
     Integer dp[][];
     
     public int change(int amount, int[] coins) {
+        return  bottomUp(amount, coins);
+    }
+    
+    public int bottomUp(int amount, int[] coins) {
+        int[][] dp = new int[coins.length+1][amount+1];
+        dp[0][0] = 1;
+        
+        for(int i=1;i<=coins.length;i++){
+            dp[i][0] = 1;
+            for(int j=1;j<=amount;j++){
+                dp[i][j] = ((j>=coins[i-1]) ? dp[i][j-coins[i-1]] : 0)
+                          + dp[i-1][j];
+            }
+        }
+        return dp[coins.length][amount];
+    }
+    
+    
+    public int topDown(int amount, int[] coins) {
         dp=new Integer[coins.length][amount+1];
         return solve(0,amount,coins);
     }
+    
+    //   0 1 2 3 4 5
+    //[1,
+    // 2,
+    // 5]
+                  
     
     int solve(int index,int target,int coins[]){
         if(target==0){
