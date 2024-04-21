5ac9926e36d73c11a347509dcf9df1eec4034b0b
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-04-21 11:14:16 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (52.59%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,40 +1,40 @@
+import java.util.*;
+
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-      int rs = 0, re = matrix.length-1, cs = 0, ce = matrix[0].length-1;
-      List<Integer> result = new ArrayList<>();
-        
-      while(rs<=re && cs<=ce){
-          
-          for(int j=cs;j<=ce;j++)
-              result.add(matrix[rs][j]);
-          rs++;
-          
-          if(rs > re)
-              continue;
-          
-          for(int i = rs;i<=re;i++)
-              result.add(matrix[i][ce]);
-          ce--;
-          
-          if(cs > ce)
-              continue;
-          
-          
-           for(int l=ce;l>=cs;l--)
-               result.add(matrix[re][l]);
-               
-          re--;
-          
-          if(rs > re)
-              continue;
-          for(int k=re;k>=rs;k--)
-              result.add(matrix[k][cs]);
-          cs++;   
-          
-      }
-      
-     
-      return result;  
-         
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        List<Integer> result = new ArrayList<>();
+        int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
+
+        while (top <= bottom && left <= right) {
+            // Traverse top row
+            for (int j = left; j <= right; j++)
+                result.add(matrix[top][j]);
+            top++;
+
+            // Traverse right column
+            for (int i = top; i <= bottom; i++)
+                result.add(matrix[i][right]);
+            right--;
+
+            // Check if there are still rows left to traverse
+            if (top <= bottom) {
+                // Traverse bottom row
+                for (int j = right; j >= left; j--)
+                    result.add(matrix[bottom][j]);
+                bottom--;
+            }
+
+            // Check if there are still columns left to traverse
+            if (left <= right) {
+                // Traverse left column
+                for (int i = bottom; i >= top; i--)
+                    result.add(matrix[i][left]);
+                left++;
+            }
+        }
+
+        return result;
     }
 }
