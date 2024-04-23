b066da429093520c85a8a60e9ae958e185db2e54
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-04-23 11:28:01 +0530

    Time: 40 ms (28.04%), Space: 57 MB (40.09%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -1,59 +1,51 @@
 class Solution {
-    
-    // the core crux of the problem is if we choose such a node
-    // that's in middle of the tree, we get the height as minimum.
-    // steps
-    // pull out leaves
-    // remove leaves pull out next leaves 
-    // keep on doing so till we have left 1 or 2 node 
-    // that's the answer
-    
-    
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        if(n == 1){
-            int node = n-1;
-            return Arrays.asList(node);
+        // If there is only one node, return it as the root
+        if (n == 1) {
+            return Collections.singletonList(0);
         }
-        
+
+        // Create an adjacency list to represent the graph
         List<Set<Integer>> adjacencyList = new ArrayList<>();
-        
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             adjacencyList.add(new HashSet<>());
         }
-        
-        for(int[] edge:edges){
-            adjacencyList.get(edge[0]).add(edge[1]);
-            adjacencyList.get(edge[1]).add(edge[0]);
+
+        // Populate the adjacency list based on the given edges
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adjacencyList.get(u).add(v);
+            adjacencyList.get(v).add(u);
         }
-        
-        List<Integer> leaves = new LinkedList<>();
-        
-        for(int i = 0; i < n; i++){
-            if(adjacencyList.get(i).size() == 1){
+
+        // Find the initial leaves of the tree
+        List<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (adjacencyList.get(i).size() == 1) {
                 leaves.add(i);
             }
         }
-        
-       while(n > 2){
-           n-= leaves.size();
-           
-           List<Integer> newLeaves = new ArrayList<>();
-           
-           for(int leaf:leaves){
-               // adjacencyList.get(leaf) will have only 1 element
-               // to get element from set we need iterator
-               int neighbour = adjacencyList.get(leaf).iterator().next();
-               adjacencyList.get(neighbour).remove(leaf);
-               adjacencyList.get(leaf).remove(neighbour);
-               if(adjacencyList.get(neighbour).size() == 1){
-                   newLeaves.add(neighbour);
-               }
-           }
-           
-           leaves = newLeaves;
-       }
-        
-       return leaves; 
-    
+
+        // Trim the tree until only the roots remain
+        while (n > 2) {
+            n -= leaves.size();
+            List<Integer> newLeaves = new ArrayList<>();
+            for (int leaf : leaves) {
+                // Find the neighbor of the leaf node
+                int neighbor = adjacencyList.get(leaf).iterator().next();
+                // Remove the leaf from the neighbor's adjacency list
+                adjacencyList.get(neighbor).remove(leaf);
+                // If the neighbor becomes a leaf, add it to the new leaves list
+                if (adjacencyList.get(neighbor).size() == 1) {
+                    newLeaves.add(neighbor);
+                }
+            }
+            // Update the leaves list for the next iteration
+            leaves = newLeaves;
+        }
+
+        // The remaining nodes in the leaves list are the roots of the minimum height trees
+        return leaves;
     }
 }
