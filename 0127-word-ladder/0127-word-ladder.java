bab9efc88365aeec54ba7e52b0a310fab30bbe46
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-05-05 13:30:04 +0530

    Time: 905 ms (12.00%), Space: 45 MB (92.25%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        
+        
+        boolean contains = wordList.stream().filter(word->word.equals(endWord)).findAny().isPresent();
+        
+        if(!contains)
+            return 0;
+        
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        
+        HashSet<String> visited = new HashSet<>();
+        visited.add(beginWord);
+        
+        int level = 1;
+        
+        while(!queue.isEmpty()){
+            
+            int size = queue.size();
+            
+            for(int i=0;i < size; i++){
+              String current = queue.remove();
+              if(current.equals(endWord)){
+                return level;
+              }
+              for(String word: wordList){
+                  if(!visited.contains(word) && isNextDistance(current,word)){
+                     queue.add(word);
+                     visited.add(word);
+                 }
+              } 
+            }
+            
+            level++;
+        }
+        
+        return 0;
+    }
+    
+    
+    boolean isNextDistance(String a,String b){
+        int cnt = 0;
+        
+        for(int i=0; i < a.length(); i++){
+            if(a.charAt(i) != b.charAt(i)){
+                cnt++;
+            }
+            
+            if(cnt > 1){
+                return false;
+            }
+        }
+        
+        return cnt!=0;
+    }
+}
