173c8fbce0ed26ab608ebe021c37fccd00833c26
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-05-11 12:56:34 +0530

    Time: 397 ms (49.78%), Space: 69.4 MB (44.10%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -1,18 +1,22 @@
 
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] A, int K) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Double.compare((double)A[a[0]] / (double)A[a[1]], (double)A[b[0]] / (double)A[b[1]]));
-        pq.offer(new int[]{0, A.length - 1});
-        for (int i = 0; i < K - 1; i++) {
-            int[] cur = pq.poll();
-            if (cur[0] + 1 < A.length && cur[1] == A.length - 1) {
-                pq.offer(new int[]{cur[0] + 1, cur[1]});
-            }
-            if (cur[1] - 1 >= 0) {
-                pq.offer(new int[]{cur[0], cur[1] - 1});
-            }
-        }
-        int[] cur = pq.poll();
-        return new int[]{A[cur[0]], A[cur[1]]};
+       
+       Comparator<int[]> comparator = (a,b)->{
+          double val1 = (double)a[0]/a[1];
+          double val2 = (double)b[0]/b[1];
+          return -Double.compare(val1,val2); 
+       };
+        
+       PriorityQueue<int[]> pq = new PriorityQueue<>(comparator); 
+       
+       for(int i = 0; i < A.length; i++){
+           for(int j = i+1; j < A.length; j++){
+               pq.add(new int[]{A[i], A[j]});
+               if(pq.size()>K)
+                   pq.remove();
+           }
+       } 
+       return pq.remove();    
     }
 }
