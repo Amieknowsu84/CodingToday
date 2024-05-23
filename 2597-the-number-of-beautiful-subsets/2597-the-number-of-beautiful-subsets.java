358b757a17cf7e8eb7b46865a76d2528d3d69b10
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-05-24 00:17:57 +0530

    Time: 679 ms (38.82%), Space: 45.1 MB (29.52%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
@@ -1,34 +1,26 @@
 class Solution {
     public int beautifulSubsets(int[] nums, int k) {
-        return beautifulSubsets(nums, k, nums.length-1,new HashMap<>());
+        return countBeautifulSubsets(nums, k, nums.length - 1, new HashMap<>());
     }
-    
-    int beautifulSubsets(int[] nums, int k, int index,Map<Integer,Integer> map) {
-        if(index < 0){
-            if(!map.isEmpty()){
-                return 1;
-            }else{
-                return 0;
-            }
+
+    private int countBeautifulSubsets(int[] nums, int k, int index, Map<Integer, Integer> map) {
+        if (index < 0) {
+            return map.isEmpty() ? 0 : 1;
         }
-        
+
         int take = 0;
-        int notTake = 0;
-    
-        if(!map.containsKey(nums[index] - k) && !map.containsKey(k + nums[index])){
-            map.put(nums[index],map.getOrDefault(nums[index],0)+1);
-            
-            take = beautifulSubsets(nums,k,index-1,map);
-            
-            if(map.get(nums[index]) == 1){
-              map.remove(nums[index]);
-            }else{
-               map.put(nums[index],map.getOrDefault(nums[index],0)-1);  
+        int notTake = countBeautifulSubsets(nums, k, index - 1, map);
+
+        if (!map.containsKey(nums[index] - k) && !map.containsKey(nums[index] + k)) {
+            map.put(nums[index], map.getOrDefault(nums[index], 0) + 1);
+            take = countBeautifulSubsets(nums, k, index - 1, map);
+            if (map.get(nums[index]) == 1) {
+                map.remove(nums[index]);
+            } else {
+                map.put(nums[index], map.get(nums[index]) - 1);
             }
         }
-        
-        notTake = beautifulSubsets(nums,k,index-1,map);
-        
+
         return take + notTake;
     }
 }
