ad3b38dae4e8c62091131e6cc9bfb1dd28b47c10
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-05-24 00:15:24 +0530

    Time: 678 ms (38.82%), Space: 44.9 MB (35.24%) - LeetHub

---
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int beautifulSubsets(int[] nums, int k) {
+        return beautifulSubsets(nums, k, nums.length-1,new HashMap<>());
+    }
+    
+    int beautifulSubsets(int[] nums, int k, int index,Map<Integer,Integer> map) {
+        if(index < 0){
+            //System.out.println();
+            if(!map.isEmpty()){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+        
+        int take = 0;
+        int notTake = 0;
+        int val = 0;
+        if(!map.containsKey(nums[index] - k) && !map.containsKey(k + nums[index])){
+            //System.out.print(nums[index] +" ");
+            map.put(nums[index],map.getOrDefault(nums[index],0)+1);
+            take = beautifulSubsets(nums,k,index-1,map);
+            if(map.get(nums[index]) == 1){
+              map.remove(nums[index]);
+            }
+            else{
+               map.put(nums[index],map.getOrDefault(nums[index],0)-1);  
+            }
+        }
+        
+        notTake = beautifulSubsets(nums,k,index-1,map);
+        
+        return take + notTake;
+    }
+}
