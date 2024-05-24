59067fcaf1a0e4faf41cfc643c3b24f7af7ce52c
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-05-25 00:47:05 +0530

    Time: 1 ms (81.78%), Space: 41.5 MB (53.81%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
@@ -12,23 +12,21 @@ class Solution {
     void maxScoreWords(String[] words, char[] letters, int[] scores, int index,int score){
         if(index >= words.length){
             maxScore = Math.max(maxScore, score);
-            //System.out.println(score);
             return;
         }
         
         char[] lettersCopy = new char[letters.length];
         System.arraycopy(letters, 0,lettersCopy, 0, letters.length); 
         
-            int currentScore = canBetaken(words[index],letters,scores);
-            if(currentScore > 0){
-                //System.out.print(currentScore+" "+words[index]+" ");
-                maxScoreWords(words, letters, scores, index+1, currentScore+score);
-            }
-            System.arraycopy(lettersCopy, 0,letters, 0, letters.length);
-            maxScoreWords(words, letters, scores, index+1, score);
+        int currentScore = calculateScore(words[index],letters,scores);
+        if(currentScore > 0){
+            maxScoreWords(words, letters, scores, index+1, currentScore+score);
+        }
+        System.arraycopy(lettersCopy, 0,letters, 0, letters.length);
+        maxScoreWords(words, letters, scores, index+1, score);
     }
     
-    int canBetaken(String word,char[] letters, int[] scores){
+    int calculateScore(String word,char[] letters, int[] scores){
         int score = 0;
         for(int i=0;i<word.length();i++){
             char current = word.charAt(i);
