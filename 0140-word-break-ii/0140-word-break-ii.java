e8008dd26217c733eaabef181bdacf1847b37661
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-05-26 00:21:39 +0530

    Time: 1 ms (94.29%), Space: 41.4 MB (80.04%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -5,34 +5,30 @@ class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
         result = new ArrayList<>();
         Set<String> words = new HashSet<>(wordDict);
-        generateSentence(s,words,0,new StringBuilder(),0);
+        generateSentence(s, words, 0, new StringBuilder(), 0);
         return result;
     }
     
-    void generateSentence(String s, Set<String> wordDict,int index,StringBuilder sb,int cnt){
-       if(index >= s.length()){
-           //System.out.println(cnt+" " +sb.length()+" "+s.length());
-           if(sb.length() == s.length() + cnt-1){
-               result.add(sb.toString());
-           }
-           return;
-       }
-       
-       StringBuilder orignal = new StringBuilder(sb.toString());
+    void generateSentence(String s, Set<String> wordDict, int index, StringBuilder sb, int wordCount) {
+        if (index >= s.length()) {
+            if (sb.length() == s.length() + wordCount - 1) {
+                result.add(sb.toString());
+            }
+            return;
+        }
         
-       for(int i=index; i<s.length();i++){
-          String word = s.substring(index, i+1);
-          if(wordDict.contains(word)){
-              if(sb.length() != 0)
-                  sb.append(" "+word);
-              else
-                  sb.append(word);
-              generateSentence(s,wordDict,i+1,sb,cnt+1);
-              sb = new StringBuilder(orignal);
-          } 
-       }
+        int originalLength = sb.length();
         
+        for (int i = index; i < s.length(); i++) {
+            String word = s.substring(index, i + 1);
+            if (wordDict.contains(word)) {
+                if (sb.length() != 0) {
+                    sb.append(" ");
+                }
+                sb.append(word);
+                generateSentence(s, wordDict, i + 1, sb, wordCount + 1);
+                sb.setLength(originalLength); // backtracking
+            }
+        }
     }
-    
-    
 }
