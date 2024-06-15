6f0f56f01eb5f7fa6f7f3a28f0506fe94442d422
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-06-15 12:22:00 +0530

    Time: 1 ms (97.62%), Space: 44.9 MB (67.88%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,22 +1,36 @@
 class Solution {
+    
+    // [2,3,1,1,4]
+    // l = 3, r = 5, f = 5
+    
     public int jump(int[] nums) {
+       // idea is to use bfs in 1d array  
         
-        int[] jumps = new int[nums.length];
-        Arrays.fill(jumps, Integer.MAX_VALUE);
-        jumps[0] = 0;
-        //[2,3,1,1,4]
-        //[0,1,1,2,2]
+      //keep maintaining a window from where we can jump and upto where (l,r)
+      //initially both left and right are at zero
+      int l = 0, r = 0;
+      // and the level we are at 0 means we are at start and don't need a jump
+      int level = 0;
         
-        for(int i=0; i <nums.length; i++){
-            int maxJump = i+nums[i]; //1+3=4
-            int val = jumps[i]; // 1
-            for(int j = i+1; j<=maxJump && j<nums.length; j++){
-                jumps[j] = Math.min(jumps[j], val+1);
-            }
-        }
+      // till when we will continue jump, till we are not in the end  [condition]
+      // once we are in end i.e r in nums.length-1 no need to jump so keep jumping till < end-1  
+       while(r < nums.length-1){
+           // farthest will get us how much farther we can jump defining our r for next level
+           int farthest = 0;
+           for(int i = l; i<=r; i++){
+               farthest = Math.max(farthest, i + nums[i]);
+           }
+           
+           //what will be the left boundary i.e min start of next level
+           // it will be prev level end +1;
+           l = r + 1;
+           r = farthest;
+           
+           //proceed to next level 
+           level++;
+       }
         
-        
-        return jumps[nums.length-1];
+       return level; 
         
     }
 }
