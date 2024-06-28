f02b4a7b4d53e501798d35838d5054406b37e121
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-06-28 23:53:16 +0530

    Time: 29 ms (35.14%), Space: 65.7 MB (31.35%) - LeetHub

---
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long maximumImportance(int n, int[][] roads) {
+        int[] connections = new int[n];
+        
+        for(int[] edge: roads){
+            connections[edge[0]]++;
+            connections[edge[1]]++;
+        }
+        
+        PriorityQueue<Integer> pq = new PriorityQueue((a,b)->connections[(int)b]-connections[(int)a]);
+        for(int i=0;i<n;i++){
+          pq.add(i);   
+        }
+        
+        int val = n;
+        long sum = 0;
+        while(!pq.isEmpty()){
+            int node = pq.remove();
+            //System.out.println(connections[node]);
+            sum+= (long)connections[node]*val;
+            val--;
+        } 
+        
+        return sum;
+    }
+}
