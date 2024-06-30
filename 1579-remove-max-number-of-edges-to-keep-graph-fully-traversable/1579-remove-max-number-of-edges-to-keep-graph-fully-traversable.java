8ce3754121b3e169a8be32c5c3e1cac8b4b50468
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-07-01 00:34:52 +0530

    Time: 11 ms (84.45%), Space: 107.8 MB (31.93%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
@@ -1,101 +1,91 @@
-class DSU{
-    List<Integer> parents;
-    List<Integer> sizes;
-    int components;
-    
-    public DSU(int n){
+class DSU {
+    private int[] parents;
+    private int[] sizes;
+    private int components;
+
+    public DSU(int n) {
         components = n;
-        parents = new ArrayList<>();
-        sizes = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            parents.add(i);
-            sizes.add(1);
+        parents = new int[n];
+        sizes = new int[n];
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
+            sizes[i] = 1;
         }
     }
-    
+
     public int findParent(int u) {
-        if(parents.get(u) == u){
-            return u;
+        if (parents[u] != u) {
+            parents[u] = findParent(parents[u]);
         }
-        int parent = findParent(parents.get(u));
-        parents.set(u,parent);
-        return parent;
+        return parents[u];
     }
-    
-    public void union(int u,int v){
-       int pu = findParent(u);
-       int pv = findParent(v);
-       
-       if(pu == pv){
-           return;
-       } 
-        
-       if(sizes.get(pu) > sizes.get(pv)){
-           sizes.set(pu,(sizes.get(pu) + sizes.get(pv)));
-           parents.set(pv,pu);          
-       }else{
-           sizes.set(pv,(sizes.get(pv) + sizes.get(pu)));
-           parents.set(pu,pv);  
-       }
-       
-       components--;
-        
+
+    public boolean union(int u, int v) {
+        int pu = findParent(u);
+        int pv = findParent(v);
+        if (pu == pv) {
+            return false;
+        }
+
+        if (sizes[pu] > sizes[pv]) {
+            sizes[pu] += sizes[pv];
+            parents[pv] = pu;
+        } else {
+            sizes[pv] += sizes[pu];
+            parents[pu] = pv;
+        }
+
+        components--;
+        return true;
+    }
+
+    public int getComponents() {
+        return components;
     }
 }
 
 class Solution {
     public int maxNumEdgesToRemove(int n, int[][] edges) {
-       DSU aliceDSU = new DSU(n);
-       DSU bobDSU = new DSU(n);
-        
-       Arrays.sort(edges,(a,b)->b[0]-a[0]);
-       
-       int usedEdges = 0; 
-       int type = -1;
-       int u = -1;
-       int v = -1;
-       boolean edgeUsed = false;
-        
-       for(int[] edge: edges){
-           type = edge[0];
-           u = edge[1]-1;
-           v = edge[2]-1;
-           edgeUsed = false;
-           
-           if(type == 3){
-               if(aliceDSU.findParent(u) != aliceDSU.findParent(v)){
-                   aliceDSU.union(u,v);
-                   edgeUsed = true;
-               }
-               
-               if(bobDSU.findParent(u) != bobDSU.findParent(v)){
-                   bobDSU.union(u,v);
-                   edgeUsed = true;
-               }   
-           }else if(type == 2){
-                if(bobDSU.findParent(u) != bobDSU.findParent(v)){
-                   bobDSU.union(u,v);
-                   edgeUsed = true;
-               }  
-               
-           }else{
-               if(aliceDSU.findParent(u) != aliceDSU.findParent(v)){
-                   aliceDSU.union(u,v);
-                   edgeUsed = true;
-               } 
-           }
-           
-           if(edgeUsed){
-              usedEdges++; 
-           }
-       }
-        
-       if(aliceDSU.components == 1 && bobDSU.components == 1){
-           return edges.length-usedEdges;
-       }else{
-           return -1;
-       }
-        
-        
+        DSU aliceDSU = new DSU(n);
+        DSU bobDSU = new DSU(n);
+
+        int usedEdges = 0;
+
+        // Process type 3 edges first
+        for (int[] edge : edges) {
+            if (edge[0] == 3) {
+                int u = edge[1] - 1;
+                int v = edge[2] - 1;
+                boolean aliceUnion = aliceDSU.union(u, v);
+                boolean bobUnion = bobDSU.union(u, v);
+                if (aliceUnion || bobUnion) {
+                    usedEdges++;
+                }
+            }
+        }
+
+        // Process type 1 and 2 edges
+        for (int[] edge : edges) {
+            int type = edge[0];
+            int u = edge[1] - 1;
+            int v = edge[2] - 1;
+
+            if (type == 1) {
+                if (aliceDSU.union(u, v)) {
+                    usedEdges++;
+                }
+            } else if (type == 2) {
+                if (bobDSU.union(u, v)) {
+                    usedEdges++;
+                }
+            }
+        }
+
+        // Check if both Alice and Bob can traverse the graph
+        if (aliceDSU.getComponents() == 1 && bobDSU.getComponents() == 1) {
+            return edges.length - usedEdges;
+        } else {
+            return -1;
+        }
     }
 }
