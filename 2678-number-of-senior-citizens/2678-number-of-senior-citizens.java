3639a3633ed161ca29e84d8c2092b0f4985c698a
Aman Singh <58386090+Amieknowsu84@users.noreply.github.com>
2024-08-02 08:24:06 +0530

    Time: 5 ms (10.35%), Space: 42.6 MB (46.14%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -1,13 +1,16 @@
 class Solution {
     public int countSeniors(String[] details) {
-        int seniorCount = 0;
-        for (String info : details) {
+        
+        Predicate<String> isGreaterThan60 = info -> {
             char tens = info.charAt(11);
             char ones = info.charAt(12);
-            if (tens > '6' || (tens == '6' && ones > '0')) {
-                seniorCount++;
-            }
-        }
-        return seniorCount;
+            return (tens > '6' || (tens == '6' && ones > '0')) ? true : false;
+        };
+        
+       long seniorCount =  Arrays.stream(details).
+            filter(detail -> isGreaterThan60.test(detail))
+            .collect(Collectors.counting());
+        
+       return (int) seniorCount;
     }
 }
